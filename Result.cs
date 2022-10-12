3b6cc5410d0eed455a979087ece01f45c2f8ac43
yuhuan0205 <65952037+yuhuan0205@users.noreply.github.com>
2022-10-12 18:16:41 +0800

    CSVreader

---
+++ Result.cs
@@ -0,0 +1,57 @@
+﻿namespace CsvReaderWFCodingStyleVertion
+{
+    /// <summary>
+    /// each result are sumed by transactions which have same stockId.
+    /// </summary>
+    public class Result
+    {
+        /// <summary>
+        /// Id of the stock.
+        /// </summary>
+        public string StockId { get; set; }
+
+        /// <summary>
+        /// Name of the stock.
+        /// </summary>
+        public string StockName { get; set; }
+
+        /// <summary>
+        /// BuyQty's sum of the stock.
+        /// </summary>
+        public int BuyTotal { get; set; }
+
+        /// <summary>
+        /// SellQty's sum of the stock.
+        /// </summary>
+        public int SellTotal { get; set; }
+
+        /// <summary>
+        /// Average price of the stock.
+        /// </summary>
+        public decimal AvgPrice { get; set; }
+
+        /// <summary>
+        /// BuyTotal - SellTotal.
+        /// </summary>
+        public int BuySellOver { get; set; }
+
+        /// <summary>
+        /// How many SecBroker buy or sell this stock.
+        /// </summary>
+        public int SecBrokerCnt { get; set; }
+
+        /// <summary>
+        /// constructor.
+        /// </summary>
+        public Result()
+        {
+            StockId = string.Empty;
+            StockName = string.Empty;
+            BuySellOver = 0;
+            SellTotal = 0;
+            AvgPrice = 0;
+            BuySellOver = 0;
+            SecBrokerCnt = 0;
+        }
+    }
+}
---
+++ TablesInfo.cs
@@ -0,0 +1,73 @@
+﻿namespace CsvReaderWFCodingStyleVertion
+{
+    /// <summary>
+    /// this class store some static values about columns.
+    /// </summary>
+    public static class TableInfo
+    {
+        /// <summary>
+        /// a valid csv should has 8 columns.
+        /// </summary>
+        public const int DOCUMENT_COLUMN_LENTH = 8;
+
+        /// <summary>
+        /// string All.
+        /// </summary>
+        public const string ALL = "All";
+
+        /// <summary>
+        /// transaction column 0
+        /// </summary>
+        public const int DEAL_DATE = 0;
+
+        /// <summary>
+        /// transaction column 1
+        /// </summary>
+        public const int STOCK_ID = 1;
+
+        /// <summary>
+        /// transaction column 2
+        /// </summary>
+        public const int STOCK_NAME = 2;
+
+        /// <summary>
+        /// transaction column 3
+        /// </summary>
+        public const int SEC_BROKER_ID = 3;
+
+        /// <summary>
+        /// transaction column 4
+        /// </summary>
+        public const int SEC_BROKER_NAME = 4;
+
+        /// <summary>
+        /// transaction column 5
+        /// </summary>
+        public const int PRICE = 5;
+
+        /// <summary>
+        /// transaction column 6
+        /// </summary>
+        public const int BUT_QTY = 6;
+
+        /// <summary>
+        /// transaction column 7
+        /// </summary>
+        public const int SELL_QTY = 7;
+
+        /// <summary>
+        /// column's name to column's index.
+        /// </summary>
+        //public enum TransactionField : int
+        //{
+        //    DealDate = 0,
+        //    StockId = 1,
+        //    StockName = 2,
+        //    SecBrokerId = 3,
+        //    SecBrokerName = 4,
+        //    Price = 5,
+        //    BuyQty = 6,
+        //    SellQty = 7
+        //}
+    }
+}
