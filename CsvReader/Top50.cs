c19162e349edb3479b252d96089464f3b610e26d
yuhuan0205 <yuhuan0205@gmail.com>
2022-10-15 22:11:29 +0800

    adding folder

---
+++ CsvReader/Top50.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CsvReaderWFCodingStyleVertion
+{
+    /// <summary>
+    /// a object store the sum of buys and sells by the secBroker.
+    /// </summary>
+    public class Top50 : IComparable<Top50>
+    {
+        /// <summary>
+        /// Name of stock.
+        /// </summary>
+        public string StockName { get; set; }
+
+        /// <summary>
+        /// Name of secBroker.
+        /// </summary>
+        public string SecBrokerName { get; set; }
+
+        /// <summary>
+        /// buys - sells.
+        /// </summary>
+        public int BuySellOver { get; set; }
+
+        /// <summary>
+        /// a index to recognize which top50 object show early.
+        /// </summary>
+        private int SortIndex;
+
+        /// <summary>
+        /// constructor.
+        /// </summary>
+        /// <param name="transaction">a Transaction</param>
+        /// <param name="index">a index which represent the priority to sort.</param>
+        public Top50(Transaction transaction, int index)
+        {
+            StockName = transaction.StockName;
+            SecBrokerName = transaction.SecBrokerName;
+            BuySellOver = Convert.ToInt32(transaction.BuyQty) - Convert.ToInt32(transaction.SellQty);
+            SortIndex = index;
+        }
+
+        /// <summary>
+        /// to compare this to other object by BuySellOver, if they equal, then compare their SortIndex.
+        /// </summary>
+        /// <param name="other">a top50 object will compare to this object.</param>
+        /// <returns></returns>
+        public int CompareTo(Top50 other)
+        {
+            int result = other.BuySellOver.CompareTo(this.BuySellOver);
+            // they are equal.
+            if (result == 0)
+            {
+                if (this.BuySellOver >= 0)
+                {
+                    result = this.SortIndex.CompareTo(other.SortIndex);
+                }
+                else
+                {
+                    result = other.SortIndex.CompareTo(this.SortIndex);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ CsvReader/TransactionsMiner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CsvReaderWFCodingStyleVertion
+{
+    /// <summary>
+    /// give transactions and querys, then output results for rendering.
+    /// </summary>
+    public class TransactionsMiner
+    {
+        /// <summary>
+        /// sum buys, sells, secBrokers, and averge the prices out by stockId from selected transactions.
+        /// </summary>
+        /// <param name="querys">unique querys from QueryParser</param>
+        /// <returns>a Tuple contains a List of Results and a List of selected Transactions</returns>
+        public static Tuple<List<Result>, List<Transaction>> GetResults(HashSet<string> querys)
+        {
+            List<Transaction> selectedTransactions = new List<Transaction>();
+            List<Result> results = new List<Result>();
+            //ensure each SecBroker is unique.
+            HashSet<string> SecBrokerCnt = new HashSet<string>();
+
+            Dictionary<string, List<Transaction>> stockIdToTransactions = CsvDataBase.StockIdToTrascations;
+            foreach (string query in querys)
+            {
+                Result result = new Result();
+                result.StockId = query;
+                foreach (Transaction transaction in stockIdToTransactions[query])
+                {
+                    result.BuyTotal += transaction.BuyQty;
+                    result.SellTotal += transaction.SellQty;
+                    //加權平均
+                    result.AvgPrice += (transaction.Price * transaction.BuyQty) +
+                        (transaction.Price * transaction.SellQty);
+                    result.StockName = transaction.StockName;
+                    SecBrokerCnt.Add(transaction.SecBrokerId);
+                    selectedTransactions.Add(transaction);
+                }
+
+                result.BuySellOver = result.BuyTotal - result.SellTotal;
+                result.SecBrokerCnt = SecBrokerCnt.Count;
+                if ((result.BuyTotal + result.SellTotal) == 0)
+                {
+                    result.AvgPrice = 0;
+                }
+                else
+                {
+                    result.AvgPrice = result.AvgPrice / (result.BuyTotal + result.SellTotal);
+                }
+                results.Add(result);
+                SecBrokerCnt.Clear();
+            }
+            Tuple<List<Result>, List<Transaction>> output = new Tuple<List<Result>, List<Transaction>>(results, selectedTransactions);
+            return output;
+        }
+    }
+}
